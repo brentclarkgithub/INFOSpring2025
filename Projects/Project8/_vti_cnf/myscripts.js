50fb35e12caf659d3b59e9c21d1e45f7a9d0ca69
brentclarkgithub <brent.clark@ucas-edu.net>
2024-12-12 11:38:36 -0700

    added projects and practices folder

---
+++ Projects/Project8/_vti_cnf/myscripts.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Nov 2018 20:48:19 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|UCAS2\\buffingtona
+vti_modifiedby:SR|UCAS2\\buffingtona
+vti_timecreated:TR|14 Nov 2018 20:48:19 -0000
+vti_cacheddtm:TX|14 Nov 2018 20:48:19 -0000
+vti_filesize:IR|1036
+vti_backlinkinfo:VX|favorites.htm aboutus.htm birds.dwt home.htm surveypage.htm tributes.htm
